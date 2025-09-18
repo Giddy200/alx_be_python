45dd818b0714bab150bd47a0b20cd47a996afbd2
Opeyemi Ogunfeyimi <gideongifty200@gmail.com>
2025-09-18 18:49:52 +0100

    a Python scripts coverts hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,7 @@
+# A variable that define the number of hours
+hours = 2
+# Calculate the number of seconds in the given hours
+# There are 60 minutes in an hour and 60 seconds in a minute (60 * 60 = 3600)
+seconds = hours * 3600
+# Print the result in the specified format
+print(f"{hours} hour(s) is {seconds} seconds.")
