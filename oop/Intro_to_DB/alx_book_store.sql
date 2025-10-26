5bbafca321c954c480c57047a2b612fb09b7749f
Opeyemi Ogunfeyimi <gideongifty200@gmail.com>
2025-10-26 19:43:15 +0100

    "alx book store"

--- oop/Intro_to_DB/alx_book_store.sql
+++ oop/Intro_to_DB/alx_book_store.sql
@@ -1,21 +1,47 @@
-import mysql.connector
-try:
-# Connect to MySQL server
-mydb = mysql.connector.connect(host="localhost",user="root",password="swankonit375$")
-# Create cursor
-mycursor = mydb.cursor()
+-- CREATE DATABASE
+CREATE DATABASE IF NOT EXISTS alx_book_store;
 
-# Create database if not exists
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-print("Database 'alx_book_store' created successfully!")
+-- SELECT DATABASE
+USE alx_book_store;
 
-except mysql.connector.Error as err:
-    print(f"Error: {err}")
+-- Authors TABLE
+CREATE TABLE IF NOT EXISTS Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
 
-finally:
-   # Close connection
-       if 'mycursor' in locals():
-            mycursor.close()
-        mydb.close()  
-        if 'mydb' in locals(): and mydb.is_connected():
-            mydb.close()          
+-- Books TABLE
+CREATE TABLE IF NOT EXISTS Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES AUTHORS(author_id)
+);
+
+-- Customers TABLE
+CREATE TABLE IF NOT EXISTS Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- Orders TABLE
+CREATE TABLE IF NOT EXISTS Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Order_Details TABLE
+CREATE TABLE IF NOT EXISTS Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
